f1142b783238c414dc982454d7341c8bf50ae1dd
Apoorva Upadhyaya <71746182+apoorva-upadhyaya@users.noreply.github.com>
2021-07-07 00:19:42 +0530

    Add files via upload

---
+++ tweet_collection/live_data/getTweets.py
@@ -0,0 +1,47 @@
+import tweepy
+from tweepy.api import API
+import json
+import datetime
+import time
+
+
+API_KEY = ''
+API_SECRET = ''
+ACCESS_TOKEN = ''
+ACCESS_TOKEN_SECRET = ''
+
+
+key = tweepy.OAuthHandler(API_KEY, API_SECRET)
+key.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+
+
+status_count=0
+file_count=0
+class Stream2Screen(tweepy.StreamListener):
+    def on_status(self, status):
+        global status_count,file_count
+        status_count = status_count + 1
+        ts=time.time()
+        st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d')
+        if status_count%160000==0:
+            file_count=file_count+1
+        #print(status._json)
+        with open('DataCollect/Data%s_File_%s.json'%(st,file_count), 'a') as file:
+            json.dump(status._json, file)
+            file.write('\n')
+
+paths = 'single_keywords.txt'
+
+while 1:
+	try:
+        	stream = tweepy.streaming.Stream(key, Stream2Screen())
+        	#stream.filter(track=[""], languages=['en'])
+        	stream.filter(track=open(paths,'r'), languages=['en'])
+	except:
+		continue
+
+
+
+
+
